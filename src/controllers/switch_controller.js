e7f7da4d3ed0432e86c6e4451c3f7fbd995c0a00
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-05-08 20:47:06 -0300

    automatted commit

--- src/controllers/switch_controller.js
+++ src/controllers/switch_controller.js
@@ -16,8 +16,22 @@ export default class extends Controller {
     this.counterTarget.innerText = 7 
   }
 
+  dispatch(event){
+    if (event.currentTarget.checked) {
+        this.decrementCounter()
+    }
+    else {
+        this.incrementCounter()
+    }
+  }
+
   decrementCounter() {
     let count = parseInt(this.counterTarget.innerText, 10);
     this.counterTarget.innerText = (count - 1)
+  }
+
+  incrementCounter() {
+    let count = parseInt(this.counterTarget.innerText, 10);
+    this.counterTarget.innerText = (count + 1)
   }
 }
