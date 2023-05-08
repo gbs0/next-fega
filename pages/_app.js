69789f4e6138951bb3d215b3e322bea960086d3c
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-05-08 18:41:39 -0300

    Stimulus is now working '

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
 import '../styles/globals.css'
+import { Application } from "@hotwired/stimulus"
+import HelloController from "../src/controllers/hello_controller"
 
-function MyApp({ Component, pageProps }) {
+
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+
+import { useEffect } from 'react'
+
+const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    window.Stimulus = Application.start()
+    Stimulus.register("hello", HelloController)
+  }, [])
+
   return <Component {...pageProps} />
 }
 
