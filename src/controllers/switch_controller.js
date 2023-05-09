4abc894ea6e09e77e0b513483aa9ae15a9e09cf3
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-05-08 21:09:43 -0300

    automatted commit

--- src/controllers/switch_controller.js
+++ src/controllers/switch_controller.js
@@ -2,14 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ['counter']
-
+  
   connect() {
-    this.setCounter()
-    this.element.querySelectorAll("#switch").forEach(checkbox => {
-        if (checkbox.checked) {
-            this.decrementCounter()
-        }
-    });
+    this.setCounter()    
   }
 
   setCounter() {
@@ -19,19 +14,37 @@ export default class extends Controller {
   dispatch(event){
     if (event.currentTarget.checked) {
         this.decrementCounter()
+        checkVictory()
     }
     else {
         this.incrementCounter()
     }
   }
 
+  checkVictory() {
+    const points  = this.counterTarget.innerText
+    if (points == "0") {
+        let modalPartial = `
+        `
+        
+    }
+  }
+
   decrementCounter() {
-    let count = parseInt(this.counterTarget.innerText, 10);
+    let count = parseInt(this.counterTarget.innerText, 10)
     this.counterTarget.innerText = (count - 1)
   }
 
   incrementCounter() {
-    let count = parseInt(this.counterTarget.innerText, 10);
+    let count = parseInt(this.counterTarget.innerText, 10)
     this.counterTarget.innerText = (count + 1)
+  }
+
+  forEachSwitchInBox() {
+    // this.element.querySelectorAll("#switch").forEach(checkbox => {
+    //     if (checkbox.checked) {
+    //         this.decrementCounter()
+    //     }
+    // });
   }
 }
